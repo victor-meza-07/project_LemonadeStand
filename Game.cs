b7a1a2e539a02da55f6601d5d68e005394c087b5
victor-meza-07 <victord.meza@gmail.com>
2020-02-14 09:17:05 -0600

    Added all necessary classes to project

---
+++ Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_3DayStarter
+{
+    class Game
+    {
+        Player player;
+        List<Day> week;//formally known as days
+        int currentDay;
+        public Game()
+        {
+            
+        }
+
+        public void amethodthatdoesstuff() 
+        {
+            //DO THINGS!
+        }
+    }
+}
---
+++ Wallet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_3DayStarter
+{
+    class Wallet
+    {
+        private double money;
+
+        // property - TBD
+        public double Money
+        {
+            get
+            {
+                return money;
+            }
+        }
+
+        public Wallet()
+        {
+            money = 20.00;
+        }
+
+        public void PayMoneyForItems(double transactionAmount)
+        {
+            money -= transactionAmount;
+        }
+    }
+}
