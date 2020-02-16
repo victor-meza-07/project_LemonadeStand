048e8bd8b5aca2cf0841c831c11d0d2c1e8fd857
victor-meza-07 <victord.meza@gmail.com>
2020-02-15 20:13:09 -0600

    Added research in game, adding formulas to calculate accurate levels for recipes

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # project_LemonadeStand
+Units And Measurements:
+Ice Cube: 29ml of Water (Average in US)
+Sugar Cube: 4g added sugar;
+Water: ml
+Acrid Levels: 1.44g/ml
+Lemon ml in Lemon: 14.7ml
+Full documentation of models:
+Distribution of Spending Power:https://www.bea.gov/data/income-saving/personal-income
+Total Income% Spend on average: https://www.bls.gov/news.release/cesan.nr0.htm
+ Distribution of Affinity to Sugar: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5039079/
+	This data was then translated to Joules and then to J/kg of sugar, to obtain total "sugar cubes" figure. assuming that all sugar is added sugar; 1/4 total grams = 1 sugarcube.
+Sugar Distribution (cntd): https://globalassets.starbucks.com/assets/867c147d5dcc46e9afb307093d5172e1.pdf
+	Grande Soy Chai Sugar Gram Content; 36.7g
+Distribution of Acridity in US for Commercial Products
+	https://www.ada.org/en/~/media/ADA/Public%20Programs/Files/JADA_The%20pH%20of%20beverages%20in%20the%20United%20States
+-Minute Made Lemonade (pH levels 2.57) - Delta Demand Curves when compared to Chai Latte
+Acrid Levels of Lemon Juice
+	https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2637791/
+	Citric Acid / juice Amount
--- Recipe.cs
+++ Recipe.cs
@@ -13,9 +13,14 @@ namespace LemonadeStand_3DayStarter
         public int amountOfIceCubes { get; set; }
         public int amountOfWater { get; set; }
         public double pricePerCup { get; set; }
-        public double AcridLevels { get { } }//Should only be Gotten never set;
-        public double SweetnessLevels { get { } }
+        public double AcridLevels { get { return acridLevels; } }//Should only be Gotten never set;
+        private double acridLevels;
+        public double SweetnessLevels { get { return sweetnessLevels; } }
+        private double sweetnessLevels;
 
+        public double CoolingFactor { get { return coolingFactor; } }
+        private double coolingFactor;
+        
         public double CostPerCup;
         public Recipe()
         {
@@ -45,6 +50,20 @@ namespace LemonadeStand_3DayStarter
         public void GetCostPerCup(double PricePerLemon, double PricePerSugarCube, double PricePerIceCube, int sizeOfPitcherInCups) 
         {
             this.CostPerCup = ((PricePerLemon * this.amountOfLemons) + (PricePerSugarCube * this.amountOfSugarCubes) + (PricePerIceCube * this.amountOfIceCubes)) / sizeOfPitcherInCups;
+        }
+        public void SetAcridityLevels() 
+        {
+
+            /*TODO
+             * Double check your mathe here for the acridity levels it should be 1.44g/ml Lemmon Juice, which would drop if
+             * acridity should drop if more grams of sugar are added. 
+             
+             */
+            if ((amountOfLemons > 0) && !(amountOfWater > 0) && !(amountOfSugarCubes > 0))
+            { acridLevels = ((amountOfLemons * 14.7) * 1.44); }//ONLY LEMON JUICE ADDED
+            else if ((amountOfLemons > 0) && !(amountOfWater > 0) && (amountOfSugarCubes > 0)) 
+            { acridLevels = (((amountOfLemons * 14.7) * 1.44)) - (amountOfSugarCubes * 4); }//Lemon J and Sugar Only
+            else { }//ALL THREE ADDED
         }
 
         //Models
